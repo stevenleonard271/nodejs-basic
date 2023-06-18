ddc0f98afa2309ae2dc7e1961c08bd49d377ed15
stevenleonard271 <stevenleonard271@gmail.com>
2023-06-18 19:09:48 +0700

    belajar node js

---
+++ events/index.js
@@ -0,0 +1,14 @@
+const {EventEmitter} = require('events');
+
+
+const birthdayEventListener = (name) => {
+    console.log(`Happy birthday ${name}!`);
+}
+
+const myEmitter = new EventEmitter();
+   
+myEmitter.on('birthday',birthdayEventListener);
+
+myEmitter.emit('birthday','Elvira');
+   
+  
